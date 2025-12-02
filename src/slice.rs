c65e1cc0c5daa44598cf879047ca75b18a06ecbc
= <hera@hera.wtf>
2025-12-01 23:32:50 -0800

    Start reworking internal macros
    The current system is... unwieldy. Time to decentralize!

--- src/slice.rs
+++ src/slice.rs
@@ -1,93 +1,180 @@
-use core::fmt;
+use crate::{marker::TypeEq, slice::private::SliceKind};
+use core::{convert::Infallible, fmt, str::Utf8Error};
 
 /// Internal implementation details.
 pub(crate) mod private;
 
-/// Marker trait the kinds of slices this crate can work with.
+/// Trait for the various slice types we support.
 ///
 /// # Safety
 ///
 /// ***TODO***
 pub unsafe trait Slice: private::Sealed {
-    /// An associated item that details what the underlying items of this
-    /// slice, are.
+    /// An associated item detailing the underlying in-memory representation
+    /// of the implementor.
     ///
-    /// The length of this slice is equivalent to the amount of `Self::Elem`s
-    /// stored within.
+    /// The length of implementor is equal to the almount of `Elem`s stored within it.
     ///
     /// # Safety
     ///
-    /// The underlying memory for a slice must be a valid `[Self::Elem]`. No exceptions.
+    /// The backing memory for this slice must be a valid `[Elem]`. No exceptions are made.
     ///
-    /// This means that the underlying memory of this slice type must be: initialized
-    /// and properly aligned `Self::Elem`s.
+    /// Therefore, `Self` must have the same alignment requirements as `[Elem]`, as well as
+    /// properly initialized for `[Elem]`.
     ///
-    /// # Validity
+    /// # Bit Validity
     ///
-    /// Not all `[Self::Elem]`s may be valid a valid `Self`.
+    /// While all `Self`s contain a `[Elem]`, not all `[Elem]`s are valid as a `Self`.
     type Elem: Sized;
 
-    /// An error that occurs when trying to decode this slice from a `[Self::Elem]`.
-    type DecodeError: Sized + fmt::Debug + fmt::Display;
+    /// An error that is returned when trying to create a `Self` from some `[Elem]`.
+    type FromElemsError: Sized + fmt::Debug + fmt::Display;
 
-    /// An error that occurs when trying to get the underlying `[Self::Elem]`
-    /// from this slice.
-    type ElemError: Sized + fmt::Debug + fmt::Display;
+    /// An error that is returned when trying to safely get a `[Elem]` from some `Self`.
+    type AsElemsError: Sized + fmt::Debug + fmt::Display;
 
-    /// An error that occurs when trying to split a slice.
+    /// An error that may occur when attempting to split this slice into a subslice.
     ///
-    /// This type does not deal with out-of-bounds situations. This instead is used
-    /// for when something is within bounds, but it did not meet some requirement.
-    ///
-    /// Types where this is the case would be [`str`], where you have to split on UTF-8
-    /// character boundaries.
+    /// This does not include out of bounds errors.
     type SplitError: Sized + fmt::Debug + fmt::Display;
 
-    // A type witness to allow const polymorphism.
-    //
-    // Downstream crates must not rely on this existing. This is purely an implementation
-    // detail, and it's removal is not considered a breaking change.
+    // Type witness.
     #[doc(hidden)]
-    const KIND: private::SliceKind<Self>;
+    const KIND: SliceKind<Self>;
 }
 
-/// Marker trait for slices that can safely be accessed immutably as a slice
-/// of their inner elements.
-///
-/// # Safety
-///
-/// Types such as `[T]`, [`str`], are examples where it is safe to access
-/// their internal buffer immutably without fear of it invalidating the
-/// invariants of the type.
-///
-/// Types that impose additional invariants upon their elements, and said elements
-/// contain interior mutability that permits invalidating those invariants,
-/// ***must not*** implement this trait.
 pub unsafe trait AsElems: Slice {}
-
-/// Marker trait for slices that can safely be accessed mutably as a slice
-/// of their inner elements.
-///
-/// # Safety
-///
-/// Types such as `[T]` is an example where it is safe to access their internal buffer
-/// mutably without fear of it invalidating the invariants of the type.
-///
-/// Types that impose additional invariants upon their elements, and said elements
-/// can be modified in such a manner that invalidates those invariants, ***must not***
-/// implement this trait.
-///
-/// An example of a type where this is the case is [`str`], which while just a byte slice
-/// in memory, it is one that ***must*** be valid UTF-8. If you were to safely get access
-/// to a `&mut [u8]` from a `&mut str`, it would be undefined behavior as one could modify
-/// the `&mut [u8]` in such a way that results in it no longer being UTF-8, resulting in undefined
-/// behavior.
 pub unsafe trait AsElemsMut: AsElems {}
 
-// /// An error that occurs when an index is not a character boundary.
-// #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-// pub struct StrSplitError {
-//     /// The index where we failed to split at.
-//     index: usize,
-//     /// The
-// }
+unsafe impl<S> AsElems for S where S: Slice<AsElemsError = Infallible> + ?Sized {}
+unsafe impl<S> AsElemsMut for S where S: Slice<AsElemsError = Infallible> + ?Sized {}
+
+macro_rules! get {
+    ($a:ty $(|)?) => {
+        $a
+    };
+
+    ($a:ty | $b:ty $(|)?) => {
+        $b
+    };
+}
+
+macro_rules! slice {
+    ($(
+        $(#[cfg($($cfg:tt)*)])*
+        $(#[doc = $doc:expr])*
+        unsafe impl $((  $($gen:tt)* ))? Slice
+            for $slice:ty $(| $slice_override:ty)? $(|)?
+        {
+            $(#[$elem_attr:meta])*
+            type Elem = $elem:ty $(| $elem_override:ty)? $(|)?;
+
+            $(#[$from_elems_attr:meta])*
+            type FromElemsError = $from_elems:ty $(| $from_elems_override:ty)? $(|)?;
+
+            $(#[$as_elems_attr:meta])*
+            type AsElemsError = $as_elems:ty $(| $as_elems_override:ty)? $(|)?;
+
+            $(#[$split_attr:meta])*
+            type SplitError = $split:ty $(| $split_override:ty)? $(|)?;
+
+            $(#[$variant_attr:meta])*
+            type Variant = $variant:ident;
+
+            $(#[$module_attr:meta])*
+            type Module = $module:ident;
+        }
+    )+) => {
+        $(
+            $(#[cfg($($cfg)*)])*
+            impl $(< $($gen)* >)? private::Sealed for $slice {}
+
+            $(#[cfg($($cfg)*)])*
+            $(#[doc = $doc])*
+            unsafe impl $(< $($gen)* >)? Slice for $slice {
+                $(#[$elem_attr])*
+                type Elem = $elem;
+
+                $(#[$from_elems_attr])*
+                type FromElemsError = $from_elems;
+
+                $(#[$as_elems_attr])*
+                type AsElemsError = $as_elems;
+
+                $(#[$split_attr])*
+                type SplitError = $split;
+
+                const KIND: SliceKind<Self> = SliceKind(SliceWit::$variant {
+                    slice: TypeEq::new(),
+                    elem: TypeEq::new(),
+                    from_elems_error: TypeEq::new(),
+                    as_elems_error: TypeEq::new(),
+                    split_error: TypeEq::new(),
+                });
+            }
+
+
+            $(#[$module_attr])*
+            $(#[cfg($($cfg)*)])*
+            mod $module;
+        )+
+
+        #[allow(dead_code)]
+        pub(crate) enum SliceWit<S>
+        where
+            S: Slice + ?Sized,
+        {
+            $(
+                $(#[cfg($($cfg)*)])*
+                $variant {
+                    slice: TypeEq<S, get!($slice | $($slice_override)?)>,
+                    elem: TypeEq<S::Elem, get!($elem | $($elem_override)?)>,
+                    from_elems_error: TypeEq<S::FromElemsError, get!($from_elems | $($from_elems_override)?)>,
+                    as_elems_error: TypeEq<S::AsElemsError, get!($as_elems | $($as_elems_override)?)>,
+                    split_error: TypeEq<S::SplitError, get!($split | $($split_override)?)>,
+                },
+            )+
+        }
+
+        impl<S> Clone for SliceWit<S>
+        where
+            S: Slice + ?Sized,
+        {
+            #[inline(always)]
+            fn clone(&self) -> SliceWit<S> {
+                *self
+            }
+        }
+
+        impl<S> Copy for SliceWit<S>
+        where
+            S: Slice + ?Sized,
+        {}
+    };
+}
+
+slice! {
+    unsafe impl(T) Slice for [T] | [S::Elem]  {
+        type Elem = T | S::Elem;
+
+        type FromElemsError = Infallible;
+        type AsElemsError = Infallible;
+        type SplitError = Infallible;
+
+        type Variant = Slice;
+        type Module = slice;
+    }
+
+    unsafe impl Slice for str {
+        type Elem = u8;
+
+        type FromElemsError = Utf8Error;
+        type AsElemsError = &'static str;
+        type SplitError = &'static str;
+
+        type Variant = Str;
+        type Module = str;
+    }
+}
+
+unsafe impl AsElems for str {}
